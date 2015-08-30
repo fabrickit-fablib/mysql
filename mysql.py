7a51534efe530d59afdfebe2547aad1e0bad236c
Shunya Kitada <syun.kitada@gmail.com>
2015-08-30 20:28:40 +0900

    updated

--- mysql.py
+++ mysql.py
@@ -1,15 +1,23 @@
 # coding: utf-8
 
-from fabkit import filer, api, sudo
+import socket
+from fabkit import filer, api, sudo, env
 from fablib.base import SimpleBase
 
 
 class MySQL(SimpleBase):
     def __init__(self):
         self.data_key = 'mysql'
+        self.data = {
+            'port': 3306,
+            'users': {},
+            'databases': {},
+        }
+
         self.services = {
             'CentOS Linux 7.*': ['mysqld'],
         }
+
         self.packages = {
             'CentOS Linux 7.*': [
                 {
@@ -20,18 +28,19 @@ class MySQL(SimpleBase):
             ],
         }
 
-        self.data = {
-            'port': 3306,
-            'users': {},
-            'databases': {},
-        }
+    def init_data(self):
+        for cluster in self.data.values():
+            if env.host in cluster['hosts']:
+                self.data['mysql_cluster'] = cluster
+                break
 
     def setup(self):
-        data = self.init()
+        self.init()
         self.install_packages()
+        cluster = self.data['mysql_cluster']
 
         filer.mkdir('/etc/mysql')
-        is_updated = filer.template('/etc/mysql/my.cnf', data=data)
+        is_updated = filer.template('/etc/mysql/my.cnf', data=self.data)
 
         self.enable_services().start_services()
         if is_updated:
@@ -46,31 +55,62 @@ class MySQL(SimpleBase):
             filer.template('/root/.my.cnf', data={'root_password': root_password})
 
         self.create_users()
-        self.create_databases()
+        self.delete_default_users()
+        if env.host == cluster['hosts'][0]:
+            self.create_databases()
+
+    def setup_slave(self):
+        self.init()
+        cluster = self.data['mysql_cluster']
+
+        if len(cluster['hosts']) == 2:
+            for host in cluster['hosts']:
+                if env.host != host:
+                    master_ip = socket.gethostbyname(host)
+                    break
+
+            slave_user = cluster['users'][cluster['slave_user']]
+            result = self.sql('show slave status')
+            if result.find(master_ip) == -1:
+                self.sql("change master to "
+                         "master_host = '{master_ip}', "
+                         "master_port=3306, "
+                         "master_user='{slave_user[user]}', "
+                         "master_password='{slave_user[password]}', "
+                         "master_auto_position=1;".format(
+                             master_ip=master_ip, slave_user=slave_user))
+
+                self.sql("start slave")
 
     def sql(self, query):
         self.init()
         return sudo('mysql -uroot -e"{0}"'.format(query))
 
     def create_users(self):
-        data = self.init()
-        for username, user in data['users'].items():
-            for dbname, db in data['databases'].items():
-                if db.get('user') == username:
-                    query = 'GRANT {privileges} ON {table} TO \'{user}\'@\'{host}\' IDENTIFIED BY \'{password}\''.format(  # noqa
-                        privileges=user.get('privileges', 'ALL PRIVILEGES'),
-                        table='{0}.*'.format(dbname),
-                        user=username,
-                        password=user['password'],
-                        host=user.get('host', 'localhost'),
-                    )
+        self.init()
+        cluster = self.data['mysql_cluster']
+        for user in cluster['users'].values():
+            for src_host in user.get('src_hosts', ['localhost']):
+                query = 'GRANT {privileges} ON {table} TO \'{user}\'@\'{host}\' IDENTIFIED BY \'{password}\''.format(  # noqa
+                    privileges=user.get('privileges', 'ALL PRIVILEGES'),
+                    table=user.get('table', '*.*'),
+                    user=user['user'],
+                    password=user['password'],
+                    host=src_host,
+                )
 
-                    self.sql(query)
+                self.sql(query)
+
+    def delete_default_users(self):
+        self.init()
+        self.sql("delete from mysql.user where user='root' and host!='localhost'")
+        self.sql("delete from mysql.user where user=''")
 
     def create_databases(self):
         data = self.init()
+        cluster = data['mysql_cluster']
         with api.warn_only():
-            for db in data['databases'].values():
-                result = self.sql('use {0}'.format(db['dbname']))
+            for db in cluster['dbs']:
+                result = self.sql('use {0}'.format(db))
                 if result.return_code != 0:
-                    self.sql('CREATE DATABASE {0} DEFAULT CHARACTER SET utf8;'.format(db['dbname']))
+                    self.sql('CREATE DATABASE {0} DEFAULT CHARACTER SET utf8;'.format(db))
