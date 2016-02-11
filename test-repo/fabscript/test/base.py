4f3308be52d9b3fe15c3f3597479e9121e160dee
Shunya Kitada <syun.kitada@gmail.com>
2016-02-11 15:37:50 +0900

    update

--- test-repo/fabscript/test/base.py
+++ test-repo/fabscript/test/base.py
@@ -1,11 +1,10 @@
 # coding: utf-8
 
-from fabkit import task, parallel, serial
+from fabkit import task, serial
 from fablib.mysql import MySQL
 
 
 @task
-@parallel
 def setup():
     mysql = MySQL()
     mysql.setup()
--- test-repo/fabscript/test_bootstrap/base.py
+++ test-repo/fabscript/test_bootstrap/base.py
@@ -1,11 +1,12 @@
 # cording: utf-8
 
-from fabkit import task
+from fabkit import task, parallel
 from fablib.test_bootstrap import Libvirt
 
 libvirt = Libvirt()
 
 
 @task
+@parallel
 def setup():
     libvirt.setup()
